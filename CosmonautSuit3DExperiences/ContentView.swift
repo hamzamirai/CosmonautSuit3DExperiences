1a351403f9bd8f75650095a40e4284c46d72b56e
Hamza Chaouki <hamzachaouki23@gmail.com>
2024-04-25 23:10:33 +0100

    Initial Commit

---
+++ CosmonautSuit3DExperiences/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CosmonautSuit3DExperiences
+//
+//  Created by Hamza Chaouki on 25/4/2024.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ CosmonautSuit3DExperiences/CosmonautSuit3DExperiencesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CosmonautSuit3DExperiencesApp.swift
+//  CosmonautSuit3DExperiences
+//
+//  Created by Hamza Chaouki on 25/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct CosmonautSuit3DExperiencesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
